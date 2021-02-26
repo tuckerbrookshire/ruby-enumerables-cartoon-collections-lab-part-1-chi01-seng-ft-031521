8d0176b5fd9913b572bccdc6dd4e7fec0671bf3b
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-26 05:11:59 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,17 @@
 def greet_characters(array)
+  array.each do |element|
+    puts "Hello " + element + "!"
+    
+  end
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
 end
 
 def list_dwarves(array)
+  array.each_with_index do |item, index |
+    puts "#{index + 1}. #{item}"
+  end
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element

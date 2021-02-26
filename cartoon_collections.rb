d8e14a052f20b7d4e35fca28c458f4ebb0413de0
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-26 05:06:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,6 +10,8 @@ end
 
 def list_dwarves(array)
   array.each_with_index {|item, index |
+    index + item
+  }
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element

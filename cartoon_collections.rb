6f1bcf3394d711aab307cb572bf3e30617f7de0c
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-26 05:03:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index do |element|
+  array.each_with_index do |element, |
     puts element
    end
   # Use `each_with_index` to enumerate over the provided array
